d23600c01dc1c0338c676b66656b558af3f4a871
Annely Kurg <annely.kurg@khk.ee>
2021-11-18 09:27:26 +0200

    OOJS 2, 18.11

--- harjutus.js
+++ harjutus.js
@@ -1,3 +1,5 @@
+//tund 11.11
+
 /*function createNewPerson(name) {
     const obj = {};
     obj.name = name;
@@ -6,7 +8,7 @@
     };
     return obj;
 }*/
-function Person(name) {
+/* function Person(name) {
     this.name = name;
     this.greeting = function() {
         alert('Hi! I\'m ' + this.name + '.');
@@ -17,10 +19,58 @@ salva.name;
 salva.greeting(); */
 
 
-let person1 = new Person('Bob');
+/*let person1 = new Person('Bob');
 let person2 = new Person('Sarah');
 
 person1.name
 person1.greeting()
 person2.name
-person2.greeting();
+person2.greeting();*/
+
+//tund 18.11
+
+/*function Shape(name, sides, sideLength) {
+    //atributes - fields
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+    //functions - methods
+    this.calcPerimeter = function (){
+        //console.log(this.sides * this.sideLength)
+        this.perimeter = this.sides * this.sideLength
+        console.log(this.perimeter)
+    }
+}
+
+// Write your code below here
+let square = new Shape("square", 4, 5)
+
+console.log(square)
+let triangle = new Shape("triangle", 3, 3)
+console.log(triangle)
+
+//object method calling
+square.calcPerimeter()
+triangle.calcPerimeter()*/
+
+
+
+// object creation
+let square = new Shape("square", 4, 5)
+console.log(square)
+let triangle = new Shape("triangle", 3, 3)
+console.log(triangle)
+// object method calling
+square.calcPerimeter()
+triangle.calcPerimeter()
+
+
+
+
+
+
+
+
+
+
+
